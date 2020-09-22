2c7189a74925d492b403142194c81779004091b5
AllenChao <allen.chao@sportscity.com.vn>
2020-09-22 13:45:21 +0700

    20200922_Test1

--- AllenTest/models/models.py
+++ AllenTest/models/models.py
@@ -2,9 +2,9 @@ from odoo import models, fields, api
 from odoo.exceptions import UserError, ValidationError
 
 class DemoOdooTutorial(models.Model):
-    _name = 'demo.odoo.tutorial' #name的規範用.隔開，不要用其他的，進到DB時會自動變成demo_odoo_tutorial
-    _description = 'Demo Odoo Tutorial' #單純說明
-    _inherit = ['mail.thread', 'mail.activity.mixin'] # track_visibility  繼承
+    _name = 'demo.odoo.Allen' #name的規範用.隔開，不要用其他的，進到DB時會自動變成demo_odoo_tutorial
+    _description = 'AllenTest' #單純說明
+    #_inherit = ['mail.thread', 'mail.activity.mixin'] # track_visibility  繼承
 
     name = fields.Char('Description', required=True)
 
